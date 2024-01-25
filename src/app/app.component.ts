856ec263b42e6ca0dcf8149ad562e06501ba13fc
OstapCholach <os.cholach@gmail.com>
2024-01-25 16:39:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilterComponent } from './components/filter/filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss',
+})
+export class FilterComponent {
+  @ViewChild(MatSelect) foodSelect!: MatSelect;
+
+  foods = [{ value: 'Pizza' }, { value: 'Burger' }, { value: 'Sushi' }];
+  isSaveButtonDisabled = true;
+  filterValue: string | null = null;
+  onSelectionSave(select: MatSelect): void {
+    this.filterValue = select.value;
+    select.close();
+  }
+
+  onSelectChange(e: any) {
+    this.isSaveButtonDisabled = e.value === null;
+  }
+
+  resetSelect(select: MatSelect): void {
+    select.writeValue(null);
+    select.close();
+    this.isSaveButtonDisabled = true;
+    this.filterValue = null;
+  }
+}
